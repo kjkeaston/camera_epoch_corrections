85f11f1f8e6e1e170748cfe031b372774cdcd462
kjkeaston <kjkeaston@gmail.com>
2018-08-24 15:28:32 -0700

    changes made for median

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const csv = require("fast-csv");
 const fs = require("fs");
-
-// Select number of seconds to adjust camera epoch
-// Use a negative to reduce epoch by n-seconds
-const numOfSecondsToAdjust = 1
-
-// Select a number of decimal places to round camera epoch time
-// Use 0 to not have any decimals
-const numOfEpochDecimals = 3
+const camera_csv = require("fast-csv");
+const nmea_csv = require("fast-csv");
 
 let allCameraCaptures = [];
+let allCameraEpoch = [];
+let allNmeaEpoch = [];
+let epochDiff = []
 
-csv
+camera_csv
   .fromPath("test_camera.csv", {headers : true})
   .on("data", data => {
     let cameraRowData = {};
@@ -19,17 +15,48 @@ csv
       cameraRowData[current_key] = parseFloat(data[current_key])
       });
     allCameraCaptures.push(cameraRowData)
+    allCameraEpoch.push(cameraRowData.epoch)
   })
   .on("end", () => {
-    console.log("Done with CSV import");
+    console.log("Done with Camera CSV import");
 
-    let adjustTime = a => a + numOfSecondsToAdjust;
+    nmea_csv
+      .fromPath("test_nmea.csv", {headers : true})
+      .on("data", data => {
+        let nmeaRowData = {};
+        Object.keys(data).forEach(current_key => {
+          nmeaRowData[current_key] = parseFloat(data[current_key])
+          });
+        allNmeaEpoch.push(nmeaRowData.nmea_epoch)
+      })
+      .on("end", () => {
+        console.log("Done with NMEA CSV import");
 
-    allCameraCaptures = allCameraCaptures.map(eachCapture => ({ ...eachCapture, epoch: adjustTime(parseFloat(eachCapture.epoch.toFixed(numOfEpochDecimals))) }));
+        for (let i = 0; i < allNmeaEpoch.length; i++) {
+          epochDiff.push(allNmeaEpoch[i] - allCameraEpoch[i])
+        }
 
-    // Create csv file in current directory
-    const ws = fs.createWriteStream("camera_corrected.csv");
-    csv
-      .write(allCameraCaptures, {headers: true})
-      .pipe(ws);
- });
+        // Find median of epoch differences
+        let median;
+        let sortedEpochArr = epochDiff.sort(function(a, b) {return a - b});
+        // handle if array of all epochs is an even length >>> i.e. no middle value
+        // if no middle value, then find average of the 2 middle floats
+        if(sortedEpochArr.length % 2 === 0) {
+          let medianLeft = sortedEpochArr[sortedEpochArr.length / 2 - 1];
+          let medianRight = sortedEpochArr[sortedEpochArr.length / 2];
+          median = (medianLeft + medianRight) / 2;
+        } else {
+          // if array length is odd, then use middle value of sorted array
+          median = sortedEpochArr[Math.floor(sortedEpochArr.length/2)];
+        }
+
+        let adjustTime = a => a + median;
+        allCameraCaptures = allCameraCaptures.map(eachCapture => ({ ...eachCapture, epoch: adjustTime(eachCapture.epoch) }));
+
+        // Create csv file in current directory
+        const ws = fs.createWriteStream("camera_corrected.csv");
+        camera_csv
+          .write(allCameraCaptures, {headers: true})
+          .pipe(ws);
+    });
+  });
